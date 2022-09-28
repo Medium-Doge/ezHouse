e166a47b23a18290eab37f5c24b7f72e9dbbb258
50Fifty <jenseanfoo@gmail.com>
2022-09-28 18:26:53 +0800

    Upload server.py and API csv files

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+"""
+Pre-requisites:
+pip install scikit-learn
+pip install pandas
+"""
+
+class Server():
+    def __init__():
+        pass
+
+class RegressionTreeModel(): # inherit model class
+    def __init__():
+        pass
+
+if __name__ == "__main__":
+    pass
