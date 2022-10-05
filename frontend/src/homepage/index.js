48595df3ab382d6cab90c06b458638dcc5ec097a
Samuel <schua045@e.ntu.edu.sg>
2022-10-05 21:16:20 +0800

    added test post request to predictprice

--- frontend/src/homepage/index.js
+++ frontend/src/homepage/index.js
@@ -1,14 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import triangle from './triangle.svg';
 import background from './background.svg';
 import { useNavigate } from "react-router-dom";
 import css from './homepage.css';
+import axios from 'axios';
 
 const Homepage = () => {
 
     let navigate = useNavigate();
-    const routeChange = () =>{
+    const routeChange = () => {
         navigate('/unauthorized');
+    }
+
+    const townarray = ['ANGMOKIO',
+        'BEDOK',
+        'BISHAN',
+        'BUKIT BATOK',
+        'BUKIT MERAH',
+        'BUKIT PANJANG',
+        'BUKIT TIMAH',
+        'CENTRAL AREA',
+        'CHOA CHU KANG',
+        'CLEMENTI',
+        'GEYLANG',
+        'HOUGANG',
+        'JURONG EAST',
+        'JURONG WEST',
+        'KALLANG/WHAMPOA',
+        'MARINE PARADE',
+        'PASIR RIS',
+        'PUNGGOL',
+        'QUEENSTOWN',
+        'SEMBAWANG',
+        'SENGKANG',
+        'SERANGOON',
+        'TAMPINES',
+        'TOA PAYOH',
+        'WOODLANDS',
+        'YISHUN'];
+        
+
+    const [values, setValues] = useState({
+        postal_code: '',
+        storey_range: '',
+        town: '',
+        flat_type: ''
+    });
+
+    const handlePostalCodeChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            postal_code: event.target.value,
+        }));
+    };
+
+    const handleFlatStoreyChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            storey_range: event.target.value,
+        }));
+    };
+
+    const handleTownChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            town: event.target.value,
+        }));
+    };
+
+    const handleFlatTypeChange = (event) => {
+        event.persist();
+        setValues((values) => ({
+            ...values,
+            flat_type: event.target.value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(values);
+        axios.get('http://54.255.164.208:5000/predict?postal_code=' + values.postal_code + '&town=' + values.town + '&flat_type=' + values.flat_type + '&storey_range=' + values.storey_range)
+        .then(res => {
+        const persons = res.data;
+        console.log("boo");
+        console.log(res.data);
+        console.log(res);
+      })
     }
 
     return (
@@ -21,28 +101,30 @@ const Homepage = () => {
                 <div class="homepage_bodyt1">Locate your dream home</div>
                 <div>One stop service to locate your dream home in Singapore with Artificial Intelligence price predictor</div>
 
-                <form class="form-inline homepage_form" action="/action_page.php">
-                <input type="text" class="fontAwesome form_element text" name="postalcode" placeholder="&#xF002; Enter postal code" value=""/>
-                    <select id="flatstorey" class="form_element select">
+                <form class="form-inline homepage_form" onSubmit={handleSubmit}>
+                    <input type="text" class="fontAwesome form_element text" name="postalcode" placeholder="&#xF002; Enter postal code" value={values.postalcode} onChange={handlePostalCodeChange} />
+                    <select id="flatstorey" class="form_element select" onChange={handleFlatStoreyChange}>
                         <option value="" disabled selected hidden>FLAT STOREY</option>
                         <option value="red">Red</option>
                         <option value="orange">Orange</option>
-                        <option value="yellow">Yellow</option>
+                        <option value="10TO12">10TO12</option>
                     </select>
-                    <select id="flattype" class="form_element select" placeholder="boo">
+                    <select id="flattype" class="form_element select" onChange={handleTownChange}>
                         <option value="" disabled selected hidden>TOWN</option>
-                        <option value="red">Red</option>
-                        <option value="orange">Orange</option>
-                        <option value="yellow">Yellow</option>
+                        {townarray.map((option, index) => (
+                            <option key={index} value={option}>
+                                {option}
+                            </option>
+                        ))}
                     </select>
-                    <select id="flattype" class="form_element select" placeholder="boo">
+                    <select id="flattype" class="form_element select" onChange={handleFlatTypeChange}>
                         <option value="" disabled selected hidden>FLAT TYPE</option>
                         <option value="red">Red</option>
                         <option value="orange">Orange</option>
-                        <option value="yellow">Yellow</option>
+                        <option value="EXECUTIVE">EXECUTIVE</option>
                     </select>
                     <div class="homepage_formbuttonwrap">
-                        <button class="fontAwesome homepage_formbutton" type="submit form_element" onClick={routeChange}>SEARCH &nbsp;&#xF061;</button>
+                        <button class="fontAwesome homepage_formbutton" type="submit form_element" >SEARCH &nbsp;&#xF061;</button>
 
                     </div>
                 </form>
