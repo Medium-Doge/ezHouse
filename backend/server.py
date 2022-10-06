4a5a8261ad570d99a8e934d73b452726368168ed
50Fifty <jenseanfoo@gmail.com>
2022-10-06 21:52:41 +0800

    Changed port from 5000 to 443 for testing

--- backend/server.py
+++ backend/server.py
@@ -187,7 +187,7 @@ class APIConnector():
 
 def main():
     server = Server(__name__)
-    server.app.run("0.0.0.0", port=5000, ssl_context=("backend/server.crt", "backend/server.key"))
+    server.app.run("0.0.0.0", port=443, ssl_context=("backend/server.crt", "backend/server.key"))
     
 if __name__ == "__main__":
     main()
