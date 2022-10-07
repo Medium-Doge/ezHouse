3a884293908288ba2871d6f41c1fa9cd5ff42c8f
50Fifty <jenseanfoo@gmail.com>
2022-10-07 20:17:25 +0800

    Removed ssl_context

--- backend/server.py
+++ backend/server.py
@@ -184,7 +184,7 @@ class APIConnector():
 
 def main():
     server = Server(__name__)
-    server.app.run("0.0.0.0", port=5000, ssl_context=("backend/server.crt", "backend/server.key"))
+    server.app.run("0.0.0.0", port=5000)
     
 if __name__ == "__main__":
     main()
