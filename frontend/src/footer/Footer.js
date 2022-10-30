8d8f7f9c7423cca922b5854499c1820ec0e6fefc
Samuel <schua045@e.ntu.edu.sg>
2022-10-30 10:17:24 +0800

    add imgs to footer

--- frontend/src/footer/Footer.js
+++ frontend/src/footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import doge from '../navbar/doge.png';
 import pikachu from './pikachu.png';
+import gengar from './gengar.png';
+import bulbasaur from './bulbasaur.png';
+import magikarp from './magikarp.webp';
 
 const Footer = () => {
     const style = {
@@ -9,7 +12,12 @@ const Footer = () => {
   return (
     <div style={{height: "150px", backgroundColor: "#4A73A1", display: "flex", alignItems: "center", borderTop: "2px solid black"}}>
         <div style={{color: "white", fontWeight: "bolder", fontSize:"24px", marginLeft: "10px", display: "flex", alignItems: "center"}}>
-            Done by Medium Doge <img src={doge} style={style}/><img src={doge} style={style}/><img src={pikachu} style={style}/><img src={doge} style={style}/><img src={doge} style={style}/>
+            Done by Medium Doge
+             <img src={gengar} style={style} title="SEAN"/>
+             <img src={bulbasaur} style={style} title="DON"/>
+             <img src={pikachu} style={style} title=""/>
+             <img src={magikarp} style={style} title="TAO"/>
+             <img src={doge} style={style} title="FAIZ"/>
         </div>
     </div>
   )
