619d6a7d1e6a9b3815d97d58372e2393d608ee02
50Fifty <jenseanfoo@gmail.com>
2022-11-01 13:42:17 +0800

    Updayed

---
+++ backend/scripts/postal_code script.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+
+hdb_info = pd.read_csv("hdb_info.csv")
+hdb_info.drop(hdb_info.columns[hdb_info.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+
+for index, row in hdb_info.iterrows():
+    hdb_info.loc[index, "address"] = "{} {}".format(row[0], row[1])
+    try:
+        hdb_info.loc[index, "postal_code"] = requests.get("https://developers.onemap.sg/commonapi/search?searchVal={} {}&returnGeom=N&getAddrDetails=Y".format(row[0], row[1])).json()["results"][0]["POSTAL"]
+    except:
+        print(row[0], row[1])
+
+hdb_info.to_csv("hdb_info.csv")
